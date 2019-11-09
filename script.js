c4655353a793f2b6a791901145ed2ecf007e37e8
Elen <elcrses@ya.ru>
2019-11-09 15:47:02 +0300

    refactoring script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const emptyNumber = 16;
-const inversions = numbers => {
+const EMPTY_NUMBER = 16;
+const BOARD_SIZE = 4;
+
+const inversionsCount = numbers => {
     let count = 0;
     for (let i = 0; i < numbers.length; i++) {
         for (let j = 0; j < numbers.length; j++) {
@@ -9,17 +11,17 @@ const inversions = numbers => {
     return count;    
 }
 const shuffle = _ => {
-    let numbers = [...new Array(15)].map((_, i) => i+1);
+    let numbers = [...new Array(EMPTY_NUMBER-1)].map((_, i) => i+1);
     for (let i = 0; i < numbers.length; i++) {
         const j = i + Math.floor(Math.random() * (numbers.length - i));
         [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
     }
-    numbers.push(emptyNumber);
+    numbers.push(EMPTY_NUMBER);
     return numbers; 
 }
 const goodShuffle = _ => {
     let numbers = shuffle();
-    while (inversions(numbers) % 2 !== 0) {
+    while (inversionsCount(numbers) % 2 !== 0) {
         numbers = shuffle();
     }
     return numbers;
@@ -27,12 +29,11 @@ const goodShuffle = _ => {
 
 const game = values => {
   let numbers = [...values];
- // numbers.push(emptyNumber);
   return {
     turn: number => {
         const i1 = numbers.indexOf(number);
-        const i2 = numbers.indexOf(emptyNumber);
-        numbers[i1] = emptyNumber;
+        const i2 = numbers.indexOf(EMPTY_NUMBER);
+        numbers[i1] = EMPTY_NUMBER;
         numbers[i2] = number;
         return numbers; 
     },
@@ -47,49 +48,50 @@ const game = values => {
 const initializeBoard = values => {
     let emptyTile = document.querySelector(".missing-tile");
     emptyTile.classList = ['tile'];
-    let lastTile = document.querySelector('[id="15"]');
+    let lastTile = document.querySelector(`[id="${EMPTY_NUMBER-1}"]`);
     lastTile.classList = ['missing-tile'];
-    lastTile.innerHTML = '';
+    lastTile.innerText = '';
     lastTile.onclick = doNothing;
     [...document.getElementsByClassName('tile')]
         .forEach((tile,i) => {
-            tile.innerHTML = values[i];
+            tile.innerText = values[i];
             tile.onclick = doNothing;
         });
 }
 const moveTile = (game15, emptyId) => {
     return e => {
-       // console.log('click', e.target.innerHTML);
         let emptyTile = document.querySelector(".missing-tile");
         let number = e.target.innerHTML;
         emptyTile.classList = ['tile'];
-        emptyTile.innerHTML = number;
+        emptyTile.innerText = number;
         e.target.classList = ['missing-tile'];
-        e.target.innerHTML = '';
-        clicks(emptyId, doNothing);
+        e.target.innerText = '';
+        setClicks(emptyId, doNothing);
         game15.turn(number * 1);
         if (!game15.isOver()) 
-            clicks(e.target.id, moveTile(game15, e.target.id));
+            setClicks(e.target.id, moveTile(game15, e.target.id));
         else {    //winAction
-            document.getElementsByTagName('p')[0].style.visibility = 'visible';
+            document.getElementById('congrats').style.display = 'block';
         }   
     }
 }
 const doNothing = _ => {}
 
-const clicks = (emptyId, action) => {
-    let i = Math.floor(emptyId / 4);
-    let j = emptyId % 4;
-    [{di:-1, dj:0, move: 'bottom'}, {di:0, dj:1, move: 'left'}, {di:1, dj:0, move: 'top'}, {di:0, dj:-1, move: 'right'}].forEach(({di,dj,move}) => {
-        if (i+di >= 0 && i+di < 4 && j+dj >=0 && j+dj < 4) {
-            const index = (i+di) * 4 + (j+dj);
-            let tile = document.querySelector(`[id="${index}"]`);
-            tile.onclick = action;
-        }
+const setClicks = (emptyId, action) => {
+    let i = Math.floor(emptyId / BOARD_SIZE);
+    let j = emptyId % BOARD_SIZE;
+    [{di:-1, dj:0, move: 'bottom'}, {di:0, dj:1, move: 'left'}, 
+     {di:1, dj:0, move: 'top'}, {di:0, dj:-1, move: 'right'}]
+        .forEach(({di,dj,move}) => {
+            if (i+di >= 0 && i+di < BOARD_SIZE && j+dj >= 0 && j+dj < BOARD_SIZE) {
+                const index = (i+di) * BOARD_SIZE + (j+dj);
+                let tile = document.querySelector(`[id="${index}"]`);
+                tile.onclick = action;
+            }
     });
 }
 const newGame = _ => {
-    document.getElementsByTagName('p')[0].style.visibility = 'hidden';
+    document.getElementById('congrats').style.display = 'none';
     let boardValues = goodShuffle();
     let game15 = game(boardValues);
     while (game15.isOver()) {
@@ -97,10 +99,10 @@ const newGame = _ => {
         game15 = game(boardValues);
     }
     initializeBoard(boardValues);
-    const emptyId = emptyNumber-1;
-    clicks(emptyId, moveTile(game15, emptyId));
+    const emptyId = EMPTY_NUMBER-1;
+    setClicks(emptyId, moveTile(game15, emptyId));
 }
 
 newGame();
 
-document.getElementsByTagName('button')[0].onclick = newGame;
+document.getElementById('btn-newgame').onclick = newGame;
